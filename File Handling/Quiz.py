067f3aface1fb3f4e11263b9b18da5c5689667e7
Prashant Rajaji <103595753+Hunter-Prash@users.noreply.github.com>
2025-06-22 05:21:01 +0530

    Add state capitals quiz generator with answer keys and questionnaires

---
+++ File Handling/Quiz.py
@@ -0,0 +1,72 @@
+
+import random
+
+
+us_states_capitals = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne"
+}
+
+keys=list(us_states_capitals.keys())
+
+for i in range(2):
+    random_keys=random.sample(keys,10)
+
+    with open(f'C:\\Users\\pctec\\OneDrive\\Desktop\\BACKEND projects\\Python Scripts\\File Handling\\Questionnaire {i+1}.txt','w') as f:
+        for j in random_keys:
+            f.write(j + '\n')
+        
+    with open(f'C:\\Users\\pctec\\OneDrive\\Desktop\\BACKEND projects\\Python Scripts\\File Handling\\Answer key {i+1}.txt','w') as f:
+        for j in random_keys:
+            f.write(us_states_capitals[j] + '\n')
+        
+        
+        
